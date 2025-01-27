dc63a5d7156f45264c6ccafd54355fa32a9d4710
Foko Junior <benitojunior2022@gmail.com>
2025-01-27 20:07:14 +0100

    update

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -18,7 +18,7 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Personal Expenses',
+      title: 'Personal  Expenses',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         fontFamily: 'Quicksand',
